4d1c8192f2ef18db71af595ad7d0fd4df319ed9b
yb789 <50587196+yb789@users.noreply.github.com>
2026-09-11 15:18:43 +0300

    Update patch-impact-token.mjs

--- patch-impact-token.mjs
+++ patch-impact-token.mjs
@@ -1,8 +1,8 @@
 import fs from 'node:fs';
 
 const file = 'index.html';
-const oldToken = '8da7d076-99e4-4478-a48d-12afafaaea5a';
-const newToken = '78f630d5-38a1-405a-ae8c-7c79dba75f93';
+const oldToken = '78f630d5-38a1-405a-ae8c-7c79dba75f93';
+const newToken = 'e6edeba2-152a-4ba3-8ff8-f08e41725695';
 
 let html = fs.readFileSync(file, 'utf8');
 html = html.split(oldToken).join(newToken);
